534233401a7f7f58f660c70dc69d5e949cb84300
rebootedCoder2 <oborimoon@gmail.com>
2024-05-15 23:48:07 +0900

    see more code challenge

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
@@ -22,6 +25,8 @@ class _VideoPostState extends State<VideoPost>
   late final VideoPlayerController _videoPlayerController;
 
   bool _isPaused = false;
+  bool _seeMore = false;
+
   final Duration _animationDuration = const Duration(milliseconds: 200);
   late final AnimationController _animationController;
 
@@ -58,6 +63,11 @@ class _VideoPostState extends State<VideoPost>
       _animationController.forward();
     }
     _isPaused = !_isPaused;
+    setState(() {});
+  }
+
+  void _onSeeMoreToogle() {
+    _seeMore = !_seeMore;
     setState(() {});
   }
 
@@ -126,6 +136,60 @@ class _VideoPostState extends State<VideoPost>
               ),
             ),
           ),
+          Positioned(
+            bottom: 40,
+            left: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "@Moon",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: Sizes.size20,
+                  ),
+                ),
+                Gaps.v5,
+                const Text(
+                  "This is Vetnam",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Sizes.size16,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "2019년 1월에..",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size14,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: _onSeeMoreToogle,
+                      child: _seeMore
+                          ? const Text(
+                              "코로나 전의 마지막 여행 넘나 아쉬움. fold",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: Sizes.size14,
+                              ),
+                            )
+                          : const Text(
+                              "See More",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: Sizes.size14,
+                              ),
+                            ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
