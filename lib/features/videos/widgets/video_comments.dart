9daffa13648944fa0d69e21efe4af00eae7a3b17
rebootedCoder2 <oborimoon@gmail.com>
2024-05-17 06:51:55 +0900

    add comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -16,29 +18,118 @@ class _VideoCommentsState extends State<VideoComments> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
+      height: size.height * 0.75,
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(Sizes.size14),
       ),
       child: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        appBar: AppBar(
           backgroundColor: Colors.grey.shade50,
-          appBar: AppBar(
-            backgroundColor: Colors.grey.shade50,
-            automaticallyImplyLeading: false,
-            title: const Text("22796 comments"),
-            actions: [
-              IconButton(
-                onPressed: _onClodePressed,
-                icon: const FaIcon(FontAwesomeIcons.xmark),
+          automaticallyImplyLeading: false,
+          title: const Text("22796 comments"),
+          actions: [
+            IconButton(
+              onPressed: _onClodePressed,
+              icon: const FaIcon(FontAwesomeIcons.xmark),
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+                vertical: Sizes.size10,
               ),
-            ],
-          ),
-          body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) =>
-                Container(child: const Text("I am a comment")),
-          )),
+              itemCount: 10,
+              separatorBuilder: (context, index) => Gaps.v20,
+              itemBuilder: (context, index) => Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 18,
+                    child: Text(
+                      "문경",
+                    ),
+                  ),
+                  Gaps.h20,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("gmapsfun • Creator",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500,
+                            )),
+                        const Text(
+                            "Pinned Location: Wharariki abc  def  lgh Beachsdklfs;lfkds;lk,"),
+                      ],
+                    ),
+                  ),
+                  Gaps.h10,
+                  Column(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size20,
+                        color: Colors.grey.shade500,
+                      ),
+                      Gaps.v2,
+                      const Text("52.2K"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              width: size.width,
+              child: BottomAppBar(
+                surfaceTintColor: Colors.white,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Colors.grey.shade400,
+                      foregroundColor: Colors.white,
+                      child: const Text(
+                        "문경",
+                      ),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                      child: TextField(
+                        cursorColor: Theme.of(context).primaryColor,
+                        decoration: InputDecoration(
+                          hintText: "Add comment...",
+                          hintStyle: TextStyle(
+                            color: Colors.grey.shade500,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(Sizes.size12),
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey.shade200,
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: Sizes.size10,
+                            horizontal: Sizes.size12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -82,6 +82,8 @@ class _VideoPostState extends State<VideoPost>
     }
     await showModalBottomSheet(
       context: context,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
       builder: (context) => const VideoComments(),
     );
     _onTogglePause();
