13424428a20bf2bdc699ca634d4aca9c7f4c3256
rebootedCoder2 <oborimoon@gmail.com>
2024-05-17 21:18:56 +0900

    discover

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,18 +1,135 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("Discover",
-            style: TextStyle(
-              color: Colors.white,
-            )),
-      ),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+          // backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text(
+              "Discover",
+            ),
+            bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              isScrollable: true,
+              tabAlignment: TabAlignment.start,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size1,
+              ),
+              indicatorColor: Colors.black,
+              labelColor: Colors.black,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+              unselectedLabelColor: Colors.grey.shade500,
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                itemCount: 20,
+                padding: const EdgeInsets.all(
+                  Sizes.size6,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                  childAspectRatio: 9 / 21,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholderFit: BoxFit.cover,
+                          placeholder: "assets/images/kyu1.jpg",
+                          image:
+                              "https://images.unsplash.com/photo-1571936804022-90d128047136?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      "This is a very long captain for my tiktok that in upload just now currently",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v5,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            radius: 12,
+                            backgroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/90151845?v=4"),
+                          ),
+                          Gaps.h4,
+                          const Expanded(
+                            child: Text(
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              "My Avatar is going to be vely long",
+                            ),
+                          ),
+                          Gaps.h4,
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade600,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "2.5M",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              for (var tab in tabs.skip(1))
+                Center(
+                  child: Text(tab,
+                      style: const TextStyle(
+                        fontSize: 28,
+                      )),
+                )
+            ],
+          )),
     );
   }
 }
