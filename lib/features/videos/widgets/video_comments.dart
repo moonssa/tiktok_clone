bf09752da10db6bb5b95489091c880e757ea7ae5
rebootedCoder2 <oborimoon@gmail.com>
2024-05-17 11:45:16 +0900

    comments section

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -12,13 +11,30 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+  final ScrollController _scrollController = ScrollController();
+
   void _onClodePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
+  }
+
+  void _onStopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return Container(
       height: size.height * 0.75,
       clipBehavior: Clip.hardEdge,
@@ -38,96 +54,143 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size16,
-                vertical: Sizes.size10,
-              ),
-              itemCount: 10,
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemBuilder: (context, index) => Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 18,
-                    child: Text(
-                      "문경",
-                    ),
+        body: GestureDetector(
+          onTap: _onStopWriting,
+          child: Stack(
+            children: [
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                    top: Sizes.size10,
+                    bottom: Sizes.size96 + Sizes.size28,
                   ),
-                  Gaps.h20,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("gmapsfun • Creator",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size14,
-                              color: Colors.grey.shade500,
-                            )),
-                        const Text(
-                            "Pinned Location: Wharariki abc  def  lgh Beachsdklfs;lfkds;lk,"),
-                      ],
-                    ),
-                  ),
-                  Gaps.h10,
-                  Column(
+                  itemCount: 10,
+                  separatorBuilder: (context, index) => Gaps.v20,
+                  itemBuilder: (context, index) => Row(
                     children: [
-                      FaIcon(
-                        FontAwesomeIcons.heart,
-                        size: Sizes.size20,
-                        color: Colors.grey.shade500,
+                      const CircleAvatar(
+                        radius: 18,
+                        child: Text(
+                          "문경",
+                        ),
                       ),
-                      Gaps.v2,
-                      const Text("52.2K"),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              width: size.width,
-              child: BottomAppBar(
-                surfaceTintColor: Colors.white,
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 18,
-                      backgroundColor: Colors.grey.shade400,
-                      foregroundColor: Colors.white,
-                      child: const Text(
-                        "문경",
+                      Gaps.h20,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("gmapsfun • Creator",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size14,
+                                  color: Colors.grey.shade500,
+                                )),
+                            const Text(
+                                "Pinned Location: Wharariki abc  def  lgh Beachsdklfs;lfkds;lk,"),
+                          ],
+                        ),
                       ),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: TextField(
-                        cursorColor: Theme.of(context).primaryColor,
-                        decoration: InputDecoration(
-                          hintText: "Add comment...",
-                          hintStyle: TextStyle(
+                      Gaps.h10,
+                      Column(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
                             color: Colors.grey.shade500,
                           ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(Sizes.size12),
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey.shade200,
-                          contentPadding: const EdgeInsets.symmetric(
-                            vertical: Sizes.size10,
-                            horizontal: Sizes.size12,
+                          Gaps.v2,
+                          const Text("52.2K"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                width: size.width,
+                child: BottomAppBar(
+                  surfaceTintColor: Colors.white,
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.grey.shade400,
+                        foregroundColor: Colors.white,
+                        child: const Text(
+                          "문경",
+                        ),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: SizedBox(
+                          height: Sizes.size40,
+                          child: TextField(
+                            onTap: _onStartWriting,
+                            expands: true,
+                            maxLines: null,
+                            minLines: null,
+                            textInputAction: TextInputAction.newline,
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              hintText: "Add comment...",
+                              hintStyle: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.shade200,
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size12,
+                              ),
+                              suffixIcon: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size10,
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    FaIcon(
+                                      FontAwesomeIcons.at,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                    Gaps.h10,
+                                    FaIcon(
+                                      FontAwesomeIcons.gift,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                    Gaps.h10,
+                                    FaIcon(
+                                      FontAwesomeIcons.faceSmile,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                    Gaps.h10,
+                                    if (_isWriting)
+                                      FaIcon(
+                                        FontAwesomeIcons.circleArrowUp,
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
