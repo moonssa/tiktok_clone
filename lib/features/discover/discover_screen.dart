52d58f00935f03ab1ee96ccbe9073c93853d7820
rebootedCoder2 <oborimoon@gmail.com>
2024-05-18 08:18:21 +0900

    discover-challenge-tab을 옮길때 키보드자판 사라지게

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -21,10 +21,12 @@ class DiscoverScreen extends StatefulWidget {
   State<DiscoverScreen> createState() => _DiscoverScreenState();
 }
 
-class _DiscoverScreenState extends State<DiscoverScreen> {
-  // final TabController _tabController = TabController(length:tabs.length, vsync: TickerPro);
+class _DiscoverScreenState extends State<DiscoverScreen>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
   final TextEditingController _textEditingController =
       TextEditingController(text: "Initial Text");
+
   void _onSearchChanged(String value) {
     print(value);
   }
@@ -34,128 +36,143 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   }
 
   @override
+  void dispose() {
+    _tabController.dispose();
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        FocusScope.of(context).unfocus();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-          // backgroundColor: Colors.white,
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            title: CupertinoSearchTextField(
-              controller: _textEditingController,
-              onChanged: _onSearchChanged,
-              onSubmitted: _onSearchSubmitted,
-            ),
-            bottom: TabBar(
-              splashFactory: NoSplash.splashFactory,
-              isScrollable: true,
-              tabAlignment: TabAlignment.start,
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size1,
-              ),
-              indicatorColor: Colors.black,
-              labelColor: Colors.black,
-              labelStyle: const TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-              unselectedLabelColor: Colors.grey.shade500,
-              tabs: [
-                for (var tab in tabs)
-                  Tab(
-                    text: tab,
-                  ),
-              ],
-            ),
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: CupertinoSearchTextField(
+            controller: _textEditingController,
+            onChanged: _onSearchChanged,
+            onSubmitted: _onSearchSubmitted,
           ),
-          body: TabBarView(
-            children: [
-              GridView.builder(
-                keyboardDismissBehavior:
-                    ScrollViewKeyboardDismissBehavior.onDrag,
-                itemCount: 20,
-                padding: const EdgeInsets.all(
-                  Sizes.size6,
+          bottom: TabBar(
+            controller: _tabController,
+            splashFactory: NoSplash.splashFactory,
+            isScrollable: true,
+            tabAlignment: TabAlignment.start,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size1,
+            ),
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
                 ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: Sizes.size10,
-                  mainAxisSpacing: Sizes.size10,
-                  childAspectRatio: 9 / 21,
-                ),
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                        Sizes.size8,
-                      )),
-                      child: AspectRatio(
-                        aspectRatio: 9 / 16,
-                        child: FadeInImage.assetNetwork(
-                            fit: BoxFit.cover,
-                            placeholderFit: BoxFit.cover,
-                            placeholder: "assets/images/kyu1.jpg",
-                            image:
-                                "https://images.unsplash.com/photo-1571936804022-90d128047136?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                      ),
-                    ),
-                    Gaps.v10,
-                    const Text(
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      "This is a very long captain for my tiktok that in upload just now currently",
-                      style: TextStyle(
-                        fontSize: Sizes.size16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v5,
-                    DefaultTextStyle(
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      child: Row(
-                        children: [
-                          const CircleAvatar(
-                            radius: 12,
-                            backgroundImage: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/90151845?v=4"),
-                          ),
-                          Gaps.h4,
-                          const Expanded(
-                            child: Text(
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              "My Avatar is going to be vely long",
-                            ),
-                          ),
-                          Gaps.h4,
-                          FaIcon(
-                            FontAwesomeIcons.heart,
-                            size: Sizes.size16,
-                            color: Colors.grey.shade600,
-                          ),
-                          Gaps.h2,
-                          const Text(
-                            "2.5M",
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              for (var tab in tabs.skip(1))
-                Center(
-                  child: Text(tab,
-                      style: const TextStyle(
-                        fontSize: 28,
-                      )),
-                )
             ],
-          )),
-    );
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            GridView.builder(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              itemCount: 20,
+              padding: const EdgeInsets.all(
+                Sizes.size6,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 21,
+              ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                      Sizes.size8,
+                    )),
+                    child: AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholderFit: BoxFit.cover,
+                          placeholder: "assets/images/kyu1.jpg",
+                          image:
+                              "https://images.unsplash.com/photo-1571936804022-90d128047136?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    "This is a very long captain for my tiktok that in upload just now currently",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v5,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/90151845?v=4"),
+                        ),
+                        Gaps.h4,
+                        const Expanded(
+                          child: Text(
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            "My Avatar is going to be vely long",
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          "2.5M",
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(tab,
+                    style: const TextStyle(
+                      fontSize: 28,
+                    )),
+              )
+          ],
+        ));
   }
 }
