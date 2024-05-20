94583cc17e5264bfd1910b5f070772f9228a03a3
rebootedCoder2 <oborimoon@gmail.com>
2024-05-20 12:05:10 +0900

    chat detail - code challenge

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -14,26 +11,63 @@ class ChatDetailScreen extends StatefulWidget {
 }
 
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  bool _isWring = false;
+
+  void _onStartWriting() {
+    setState(() {
+      _isWring = true;
+    });
+  }
+
+  void _onStopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWring = false;
+    });
+  }
+
+  void _onSubmitted(String value) {
+    setState(() {
+      _isWring = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const ListTile(
+        title: ListTile(
           contentPadding: EdgeInsets.zero,
           horizontalTitleGap: Sizes.size8,
-          leading: CircleAvatar(
-            foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/90151845?v=4"),
-            child: Text("문경"),
+          leading: Stack(
+            children: [
+              const CircleAvatar(
+                foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/90151845?v=4"),
+                radius: Sizes.size20,
+              ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  width: Sizes.size18,
+                  height: Sizes.size18,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3)),
+                ),
+              )
+            ],
           ),
-          title: Text(
+          title: const Text(
             "xxxxmmm967",
             style: TextStyle(
               fontWeight: FontWeight.w600,
             ),
           ),
-          subtitle: Text("Active now"),
-          trailing: Row(
+          subtitle: const Text("Active now"),
+          trailing: const Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               FaIcon(
@@ -51,61 +85,116 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
           ),
         ),
       ),
-      body: Stack(
-        children: [
-          ListView.separated(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
-              horizontal: Sizes.size14,
-            ),
-            itemBuilder: (context, index) {
-              final bool isMine = index % 2 == 0;
-              return Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment:
-                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(Sizes.size14),
-                    decoration: BoxDecoration(
-                      color:
-                          isMine ? Colors.blue : Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(Sizes.size20),
-                        topRight: const Radius.circular(Sizes.size20),
-                        bottomLeft: Radius.circular(
-                          isMine ? Sizes.size20 : Sizes.size5,
+      body: GestureDetector(
+        onTap: _onStopWriting,
+        child: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size20,
+                horizontal: Sizes.size14,
+              ),
+              itemBuilder: (context, index) {
+                final bool isMine = index % 2 == 0;
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment:
+                      isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(Sizes.size14),
+                      decoration: BoxDecoration(
+                        color: isMine
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(Sizes.size20),
+                          topRight: const Radius.circular(Sizes.size20),
+                          bottomLeft: Radius.circular(
+                            isMine ? Sizes.size20 : Sizes.size5,
+                          ),
+                          bottomRight: Radius.circular(
+                            isMine ? Sizes.size5 : Sizes.size20,
+                          ),
                         ),
-                        bottomRight: Radius.circular(
-                          isMine ? Sizes.size5 : Sizes.size20,
+                      ),
+                      child: const Text(
+                        "Congratulations!!",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: Sizes.size16),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Gaps.v10,
+              itemCount: 10,
+            ),
+            Positioned(
+              bottom: -10,
+              width: MediaQuery.of(context).size.width,
+              child: BottomAppBar(
+                elevation: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: Sizes.size40,
+                        // padding: const EdgeInsets.symmetric(
+                        //   horizontal: Sizes.size10,
+                        // ),
+                        child: TextField(
+                          onTap: _onStartWriting,
+                          onSubmitted: _onSubmitted,
+                          decoration: const InputDecoration(
+                            hintText: "Send a message",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(Sizes.size20),
+                                topRight: Radius.circular(Sizes.size20),
+                                bottomLeft: Radius.circular(Sizes.size20),
+                                bottomRight: Radius.circular(Sizes.size5),
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: Sizes.size10,
+                                horizontal: Sizes.size20),
+                            filled: true,
+                            fillColor: Colors.white,
+                            // contentPadding: EdgeInsets.symmetric(horizontal: Sizes.size12,),
+                            suffixIcon: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: Sizes.size10,
+                                  horizontal: Sizes.size10),
+                              child: FaIcon(
+                                FontAwesomeIcons.faceSmile,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                    child: const Text(
-                      "Congratulations!!",
-                      style: TextStyle(
-                          color: Colors.white, fontSize: Sizes.size16),
+                    Gaps.h20,
+                    Container(
+                      width: 40,
+                      height: 40,
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      child: FaIcon(
+                        FontAwesomeIcons.paperPlane,
+                        color: _isWring ? Colors.black : Colors.grey.shade500,
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
-            separatorBuilder: (context, index) => Gaps.v10,
-            itemCount: 10,
-          ),
-          Positioned(
-              bottom: 0,
-              width: MediaQuery.of(context).size.width,
-              child: const BottomAppBar(
-                  elevation: 0,
-                  child: Row(
-                    children: [
-                      Expanded(child: TextField()),
-                      Gaps.h20,
-                      FaIcon(FontAwesomeIcons.paperPlane),
-                    ],
-                  ))),
-        ],
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
