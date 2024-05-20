bee9431b2ae2f3bd89e71395d27b63023b3c8888
rebootedCoder2 <oborimoon@gmail.com>
2024-05-20 19:53:52 +0900

    sliver appBar

---
+++ lib/users/user_profile_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          snap: true,
+          pinned: true,
+          // stretch: true,
+          // pinned: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.fadeTitle,
+              StretchMode.zoomBackground,
+            ],
+            background:
+                Image.asset("assets/images/kyu1.jpg", fit: BoxFit.cover),
+            titlePadding:
+                const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+            title: const Text(
+              "Hello",
+            ),
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100,
+        ),
+      ],
+    );
+  }
+}
